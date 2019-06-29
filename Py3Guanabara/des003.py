2a23ddd91a96b22103da2f668eea80ff1f8c9f59
krn <dev.junqueira@gmail.com>
2019-06-28 23:29:29 -0300

    mundo 1

---
+++ Py3Guanabara/des003.py
@@ -0,0 +1,3 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+print(f'A soma vale {n1 + n2}')
---
+++ Py3Guanabara/des005.py
@@ -0,0 +1,2 @@
+numb = int(input('Digite um número: '))
+print(f'Analisando o valor "{numb}", temos como seu antecessor: {numb-1} e como sucessor: {numb+1}')
